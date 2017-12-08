d4af46c55e4425ad6398c25ab6b108c8ce0277fc
Matthew Bowden <bowdenm@spu.edu>
2017-12-07 18:41:59 -0800

    Add basic module and Song object skeleton

---
+++ pikaraoke/song.py
@@ -0,0 +1,15 @@
+# Author: Matthew Bowden, Nate Kaldor
+
+
+
+class Song(object):
+
+    def __init__(self, mp3_path, cdg_path):
+        """
+        Load in mp3 tag data given mp3 path
+        :param mp3_path: Path to mp3 file
+        :param cdg_path: Path to cdg file
+        """
+        pass
+
+    # define properties
