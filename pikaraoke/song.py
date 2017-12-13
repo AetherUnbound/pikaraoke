2480de09938dce08173159c94bfdbc54b31f3ec5
kaldorn <nate.kaldor@gmail.com>
2017-12-12 19:57:23 -0800

    I'm not sure how to test this, but I think it'll work. I probably referred to 8 or 9 guides and examples for how to do this before it made sense.
    What really helped was learning that I wasn't using tablib syntax correctly. Here's what I coded in a separate file to see if it worked with the Karaoke .mp3/.cdg's we'll be working with.
    
    import taglib
    
    def main():
        song = taglib.File("path")
        print(song.tags['ARTIST'], '-', song.tags['TITLE'])
        print(song)
    
    Printing the tags grabs the metadata, and printing the 'song' prints the full working file path. Knowing this should be helpful for the future.
    
    Let me know if you test this and it doesn't work.

--- pikaraoke/song.py
+++ pikaraoke/song.py
@@ -1,15 +1,41 @@
 # Author: Matthew Bowden, Nate Kaldor
-
-
+import taglib
 
 class Song(object):
 
-    def __init__(self, mp3_path, cdg_path):
+    #God I hope this is correct
+    def __init__(self, mp3_path = "", artist = "", title = ""):
         """
         Load in mp3 tag data given mp3 path
         :param mp3_path: Path to mp3 file
         :param cdg_path: Path to cdg file
         """
-        pass
+        self._song = taglib.File(mp3_path)
+        self._title = title
+        self._artist = artist
 
-    # define properties
+    @property
+    def song(self):
+        return self._song
+
+    @song.setter
+    def song(self, mp3_path):
+        self._song = taglib.File(mp3_path)
+
+    @property
+    def artist(self):
+        return self._artist
+
+    @artist.setter
+    def artist(self, mp3_path):
+        song = taglib.File(mp3_path)
+        self._artist = song.tags['ARTIST']
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, mp3_path):
+        song = taglib.File(mp3_path)
+        self._title = song.tags['TITLE']
