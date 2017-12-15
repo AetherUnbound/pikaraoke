7f69be92b674405c10175e932328316ab59b9b2b
Matthew Bowden <bowdenm@spu.edu>
2017-12-14 20:56:23 -0800

    Adjustments to Song class

--- pikaraoke/song.py
+++ pikaraoke/song.py
@@ -1,41 +1,26 @@
 # Author: Matthew Bowden, Nate Kaldor
 import taglib
 
+
 class Song(object):
 
-    #God I hope this is correct
-    def __init__(self, mp3_path = "", artist = "", title = ""):
+    def __init__(self, mp3_path, cdg_path):
         """
         Load in mp3 tag data given mp3 path
         :param mp3_path: Path to mp3 file
         :param cdg_path: Path to cdg file
         """
+        self._mp3_path = mp3_path
+        self._cdg_path = cdg_path
         self._song = taglib.File(mp3_path)
-        self._title = title
-        self._artist = artist
+        # some assertion that _mp3_path is in the same folder as _cdg_path
 
-    @property
-    def song(self):
-        return self._song
-
-    @song.setter
-    def song(self, mp3_path):
-        self._song = taglib.File(mp3_path)
-
+    # No need for setters - we're assuming these will stay constant,
+    # i.e. the song info is static and won't change
     @property
     def artist(self):
-        return self._artist
-
-    @artist.setter
-    def artist(self, mp3_path):
-        song = taglib.File(mp3_path)
-        self._artist = song.tags['ARTIST']
+        return self._song.tags['ARTIST']
 
     @property
     def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, mp3_path):
-        song = taglib.File(mp3_path)
-        self._title = song.tags['TITLE']
+        return self._song.tags['TITLE']
