3ea191071d9956c1739ecbee77466a1c8dd416f7
Nate Kaldor <kaldorn@spu.edu>
2017-12-20 19:22:35 -0800

    .mp3/.cdg folder match check
    Added an assertion to check that each song file with an .mp3 has a matching .cdg lyric file.

--- pikaraoke/song.py
+++ pikaraoke/song.py
@@ -2,6 +2,7 @@
 import taglib
 
 
+
 class Song(object):
 
     def __init__(self, mp3_path, cdg_path):
@@ -13,7 +14,9 @@ class Song(object):
         self._mp3_path = mp3_path
         self._cdg_path = cdg_path
         self._song = taglib.File(mp3_path)
+
         # some assertion that _mp3_path is in the same folder as _cdg_path
+        assert self._cdg_path == self._mp3_path.replace('.mp3', '.cdg') #This should work for that
 
     # No need for setters - we're assuming these will stay constant,
     # i.e. the song info is static and won't change
